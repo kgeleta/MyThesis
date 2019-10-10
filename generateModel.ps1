85cd1521d773ef9cba27b98ea783e798f0e22a36
Krzysztof Geleta <kgeleta@student.agh.edu.pl>
2019-10-10 15:12:39 +0200

    working version of model generating script

--- generateModel.ps1
+++ generateModel.ps1
@@ -2,13 +2,25 @@ param (
   [Parameter(Mandatory=$true, Position = 0)][string]$connectionString
 )
 
-
-mkdir Model
-cd Model
+Write-Host "Creating new project" -foregroundcolor green
+mkdir Feedback
+cd Feedback
 dotnet new classlib -f netcoreapp2.2
+
+Write-Host "Installing Entity Framework Core" -foregroundcolor green
 dotnet add package Microsoft.EntityFrameworkCore.SqlServer -v 2.2.6
 dotnet add package Microsoft.EntityFrameworkCore.Design -v 2.2.6
 
-dotnet ef dbcontext scaffold $connectionString Microsoft.EntityFrameworkCore.SqlServer -o Model1
+Write-Host "Generating model classes" -foregroundcolor green
+dotnet ef dbcontext scaffold $connectionString Microsoft.EntityFrameworkCore.SqlServer -o Model
 
 # TODO: this should be atomic (if does not complete folder should be removed)
+
+Write-Host "Copying Model directory to Assets" -foregroundcolor green
+cd ..
+Copy-Item -Path .\Feedback\Model -Destination .\Assets -Recurse
+
+Write-Host "Removing Feedback directory" -foregroundcolor green
+Remove-Item .\Feedback -Recurse
+
+Write-Host "All done" -foregroundcolor green
