296647d678519685e654231f232c6fa913d813c5
Krzysztof Geleta <kgeleta@student.agh.edu.pl>
2019-10-13 20:40:15 +0200

    added support for postgresql, sqlite and mysql

--- generateModel.ps1
+++ generateModel.ps1
@@ -1,29 +1,67 @@
 param (
-  [Parameter(Mandatory=$true, Position = 0)][string]$connectionString
+  [Parameter(Mandatory=$true, Position = 0)][string]$connectionString,
+  [Parameter(Mandatory=$true, Position = 1)][ValidateSet('sqlServer','MySql','SQLite', 'PostgreSQL')][string]$provider
 )
 
 Write-Host "Creating new project" -foregroundcolor green
 mkdir Feedback
 cd Feedback
-dotnet new classlib -f netcoreapp2.2
 
-Write-Host "Installing Entity Framework Core" -foregroundcolor green
-dotnet add package Microsoft.EntityFrameworkCore.SqlServer -v 2.2.6
-dotnet add package Microsoft.EntityFrameworkCore.Design -v 2.2.6
+try
+{
+  dotnet new classlib -f netcoreapp2.2
+  Write-Host "Installing Entity Framework Core" -foregroundcolor green
 
-Write-Host "Generating model classes" -foregroundcolor green
-dotnet ef dbcontext scaffold $connectionString Microsoft.EntityFrameworkCore.SqlServer -o Model
+  switch ($provider)
+  {
+    "sqlServer"
+    {
+      dotnet add package Microsoft.EntityFrameworkCore.SqlServer -v 2.2.6
+      dotnet add package Microsoft.EntityFrameworkCore.Design -v 2.2.6
 
-# TODO: this should be atomic (if does not complete folder should be removed)
+      Write-Host "Generating model classes" -foregroundcolor green
+      dotnet ef dbcontext scaffold $connectionString Microsoft.EntityFrameworkCore.SqlServer -o Model
+      break
+    }
+    "MySql"
+    {
+      dotnet add package MySql.Data.EntityFrameworkCore -v 8.0.17
+      dotnet add package Microsoft.EntityFrameworkCore.Design -v 2.2.6
 
-Write-Host "Copying Model directory to Assets" -foregroundcolor green
-cd ..
-Copy-Item -Path .\Feedback\Model -Destination .\Assets -Recurse -Force
+      Write-Host "Generating model classes" -foregroundcolor green
+      dotnet ef dbcontext scaffold $connectionString MySql.Data.EntityFrameworkCore -o Model
+      break
+    }
+    "SQLite"
+    {
+      dotnet add package Microsoft.EntityFrameworkCore.Sqlite -v 2.2.6
+      dotnet add package Microsoft.EntityFrameworkCore.Design -v 2.2.6
 
-Write-Host "Removing Feedback directory" -foregroundcolor green
-Remove-Item .\Feedback -Recurse
+      Write-Host "Generating model classes" -foregroundcolor green
+      dotnet ef dbcontext scaffold $connectionString Microsoft.EntityFrameworkCore.Sqlite -o Model
+      break
+    }
+    "PostgreSQL"
+    {
+      dotnet add package Npgsql.EntityFrameworkCore.PostgreSQL -v 2.2.4
+      dotnet add package Microsoft.EntityFrameworkCore.Design -v 2.2.6
 
-Write-Host "All done" -foregroundcolor green
-exit 0
+      Write-Host "Generating model classes" -foregroundcolor green
+      dotnet ef dbcontext scaffold $connectionString Npgsql.EntityFrameworkCore.PostgreSQL -o Model
+      break
+    }
+  }
+    # TODO: this should be atomic (if does not complete folder should be removed)
 
-#TODO: add error handling and exit codes
+  Write-Host "Copying Model directory to Assets" -foregroundcolor green
+
+  # this operation should be sqfe (provided that dotnet successfully generated project and Assets folder exists)
+  cd ..
+  Copy-Item -Path .\Feedback\Model -Destination .\Assets -Recurse -Force
+}
+finally
+{
+  Write-Host "Removing Feedback directory" -foregroundcolor green
+  Remove-Item .\Feedback -Recurse
+  Write-Host "All done" -foregroundcolor green
+}
