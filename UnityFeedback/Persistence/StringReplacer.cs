a69bdb1295298dc392250450dbf272f4afae5fa3
Krzysztof Geleta <kgeleta@student.agh.edu.pl>
2019-11-09 21:14:23 +0100

    Added  replacing connection string in context class

---
+++ UnityFeedback/Persistence/StringReplacer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace UnityFeedback.Persistence
+{
+	public class StringReplacer
+	{
+		/// <summary>
+		/// Contains strings to replace as pairs of original string and new string.
+		/// </summary>
+		public Dictionary<string, string> StringsToReplace = new Dictionary<string, string>();
+
+		private readonly string _pathToDirectory;
+		private readonly string _fileExtensions;
+
+		/// <summary>
+		/// Creates new instance of <see cref="StringReplacer"/>.
+		/// </summary>
+		/// <param name="pathToDirectory">Path to directory with files to replace strings in.</param>
+		/// <param name="fileExtensions">File extensions separated by semicolon. For example "*.json; *.xml; *.txt"</param>
+		public StringReplacer(string pathToDirectory, string fileExtensions)
+		{
+			this._pathToDirectory = pathToDirectory;
+			this._fileExtensions = fileExtensions;
+		}
+
+		/// <summary>
+		/// Replaces all occurrences of all <see cref="StringsToReplace"/> keys with values in files that match fileName parameter.
+		/// </summary>
+		/// <param name="fileName">Optional name of file in which replacement should be performed (supports regex). By default this will match all files.</param>
+		public void DoSwap(string fileName = @".*")
+		{
+			if (this.StringsToReplace.Count == 0)
+			{
+				return;
+			}
+
+			var fileNameRegex = new Regex(fileName);
+
+			// Get files matching regex
+			var fileNames = Directory.GetFiles(this._pathToDirectory, this._fileExtensions)
+				.Where(path => fileNameRegex.IsMatch(path))
+				.ToList();
+
+			// for each file:
+			foreach (var name in fileNames)
+			{
+				string text = File.ReadAllText(name);
+				foreach (var stringPair in this.StringsToReplace)
+				{
+					text = text.Replace(stringPair.Key, stringPair.Value);
+				}
+				File.WriteAllText(name, text);
+			}
+
+		}
+
+	}
+}
